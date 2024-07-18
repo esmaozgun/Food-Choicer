be80e1f816eed14e741c26e6298b47479a35f705
Esma Özgün <nuresma349@gmail.com>
2024-07-18 16:58:12 +0300

    Adding Navigation Bar

--- aboutmePage.dart
+++ aboutmePage.dart
@@ -1,107 +1,46 @@
 import 'package:flutter/material.dart';
-import 'dart:math' as math;
+import 'package:flutter_svg/flutter_svg.dart';
 
-class aboutmePage extends StatelessWidget {
+class NavbarWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      width: 448,
-      height: 998,
+      width: double.infinity,
+      height: 80,
       decoration: BoxDecoration(
-        color: Color.fromRGBO(255, 255, 255, 1),
+        color: Colors.white,
+        boxShadow: [
+          BoxShadow(
+            color: Colors.black.withOpacity(0.1),
+            spreadRadius: 2,
+            blurRadius: 5,
+            offset: Offset(0, 3),
+          ),
+        ],
       ),
-      child: Stack(
-        children: <Widget>[
-          Positioned(
-            top: 0,
-            left: 0,
-            child: Container(
-              width: 448,
-              height: 998,
-              decoration: BoxDecoration(
-                image: DecorationImage(
-                  image: AssetImage('assets/images/Image1.png'),
-                  fit: BoxFit.fill,
-                ),
-              ),
-            ),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: [
+          GestureDetector(
+            onTap: () {},
+            child: SvgPicture.asset('assets/images/vector1.svg'),
           ),
-          Positioned(
-            top: 114,
-            left: 25,
-            child: Container(
-              width: 396,
-              height: 447,
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(15),
-                color: Color.fromRGBO(255, 255, 255, 0.57),
-              ),
-            ),
+          GestureDetector(
+            onTap: () {},
+            child: SvgPicture.asset('assets/images/vector2.svg'),
           ),
-          Positioned(
-            top: 178,
-            left: 35,
-            child: Text(
-              'Hey there!\n'
-                  'I\'m a 20 year old software engineering intern who '
-                  '\nloves swimming, reading, and, most importantly, coding. '
-                  '\nSince I wrote my first line of code, I\'ve been hooked '
-                  '\non the endless possibilities it offers!\n'
-                  'Food Choicer is a user-friendly app for discovering'
-                  '\n delicious recipes. With a comprehensive database and '
-                  '\nintuitive interface, '
-                  '\nfinding the perfect dish is easier than ever.\n',
-              textAlign: TextAlign.center,
-              style: TextStyle(
-                color: Color.fromRGBO(0, 0, 0, 1),
-                fontFamily: 'Poppins',
-                fontSize: 15,
-                fontWeight: FontWeight.normal,
-                height: 1.2,
-              ),
-            ),
+          FloatingActionButton(
+            onPressed: () {},
+            child: Icon(Icons.add),
+            backgroundColor: Colors.green,
           ),
-          Positioned(
-            top: 452,
-            left: 90,
-            child: Text(
-              'Feel free to contact me for questions:\nnuresma349@gmail.com',
-              textAlign: TextAlign.center,
-              style: TextStyle(
-                color: Color.fromRGBO(0, 0, 0, 1),
-                fontFamily: 'Poppins',
-                fontSize: 15,
-                fontWeight: FontWeight.normal,
-                height: 1.2,
-              ),
-            ),
+          GestureDetector(
+            onTap: () {},
+            child: SvgPicture.asset('assets/images/vector3.svg'),
           ),
-          Positioned(
-            top: 68,
-            left: 23,
-            child: Container(
-              width: 397.86456298828125,
-              height: 38,
-              child: Stack(
-                children: <Widget>[
-                  Positioned(
-                    top: 0,
-                    left: 146,
-                    child: Text(
-                      'About Me',
-                      textAlign: TextAlign.left,
-                      style: TextStyle(
-                        color: Color.fromRGBO(0, 190, 41, 1),
-                        fontFamily: 'Poppins',
-                        fontSize: 20,
-                        fontWeight: FontWeight.normal,
-                        height: 1,
-                      ),
-                    ),
-                  ),
-                ],
-              ),
-            ),
+          GestureDetector(
+            onTap: () {},
+            child: SvgPicture.asset('assets/images/bg.svg'),
           ),
         ],
       ),
@@ -109,9 +48,99 @@ class aboutmePage extends StatelessWidget {
   }
 }
 
-void main() {
-  runApp(MaterialApp(
-    home: aboutmePage(),
-  ));
+class AboutMePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        width: double.infinity,
+        height: double.infinity,
+        decoration: BoxDecoration(
+          color: Color.fromRGBO(255, 255, 255, 1),
+        ),
+        child: Stack(
+          children: <Widget>[
+            Positioned.fill(
+              child: Container(
+                decoration: BoxDecoration(
+                  image: DecorationImage(
+                    image: AssetImage('assets/images/Image1.png'),
+                    fit: BoxFit.fill,
+                  ),
+                ),
+              ),
+            ),
+            Positioned(
+              top: 100,
+              left: 25,
+              right: 25,
+              child: Container(
+                width: double.infinity,
+                padding: EdgeInsets.all(16),
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(15),
+                  color: Color.fromRGBO(255, 255, 255, 0.57),
+                ),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    Text(
+                      'Hey there!',
+                      style: TextStyle(
+                        color: Colors.black,
+                        fontFamily: 'Poppins',
+                        fontSize: 18,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    SizedBox(height: 10),
+                    Text(
+                      'I\'m a 20 year old software engineering intern who loves swimming, reading, and, most importantly, coding. Since I wrote my first line of code, I\'ve been hooked on the endless possibilities it offers!\n\nFood Choicer is a user-friendly app for discovering delicious recipes. Finding the perfect dish is easier than ever.\n\nFeel free to contact me for questions:\nnuresma349@gmail.com',
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                        color: Colors.black,
+                        fontFamily: 'Poppins',
+                        fontSize: 15,
+                        fontWeight: FontWeight.normal,
+                        height: 1.2,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            Positioned(
+              top: 40,
+              left: 0,
+              right: 0,
+              child: Center(
+                child: Text(
+                  'About Me',
+                  style: TextStyle(
+                    color: Color.fromRGBO(0, 190, 41, 1),
+                    fontFamily: 'Poppins',
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+            ),
+            Positioned(
+              bottom: 0,
+              left: 0,
+              right: 0,
+              child: NavbarWidget(),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
 }
 
+void main() {
+  runApp(MaterialApp(
+    home: AboutMePage(),
+  ));
+}
